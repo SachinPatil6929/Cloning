71c0a5329a52c7c3e3f86535fc9020b64d4f889e
SachinPatil6929 <sachinraj_143@rediffmail.com>
2022-03-11 15:15:06 +0530

    Clone Commit

---
+++ src/com/prowings/javaclass/clone/Address.java
@@ -0,0 +1,27 @@
+package com.prowings.javaclass.clone;
+
+public class Address {
+	String City;
+	String State;
+	int pin;
+	
+	public Address(String city, String state, int pin) {
+		super();
+		City = city;
+		State = state;
+		this.pin = pin;
+	}
+
+	public Address() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Address [City=" + City + ", State=" + State + ", pin=" + pin + "]";
+	}
+	
+	
+	
+}
+
